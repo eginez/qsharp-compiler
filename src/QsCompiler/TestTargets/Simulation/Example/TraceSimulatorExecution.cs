9881d8332e6d7b903160406fbd6a67fe8cc5a2a7
Bettina Heim <beheim@microsoft.com>
2019-08-27 19:34:25 -0700

    adding a driver and the option to execute on the trace simulator

---
+++ src/QsCompiler/TestTargets/Simulation/Example/TraceSimulatorExecution.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Quantum.Simulation.Simulators.QCTraceSimulators;
+
+namespace Microsoft.Quantum.Testing.Simulation
+{
+    internal static class Driver
+    {
+        /// <summary>
+        /// Executes the Q# operation Main defined in Microsoft.Quantum.Testing.Simulation on the QuantumSimulator.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            var qsim = new QCTraceSimulator();
+            Simulation.Main.Run(qsim).Wait(); 
+        }
+    }
+}
